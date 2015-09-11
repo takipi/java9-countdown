cc21cf0dc1b8ceb5aa150f019ea0c43b3d443a09
Mauricio Payetta <mauricio.payetta@dianrong.com>
2015-09-11 09:21:58 +0800

    fixed bottom boxes, tweet text and minor issues

--- js/main.js
+++ js/main.js
@@ -26,18 +26,20 @@ $(function() {
 	var currentProgressWidth = (days * 100) / totalDaysProgress;
 	$('.progress-bar .progress').animate({ width: currentProgressWidth + '%' }, 2000);
 	
+	var daysJdkModular = 115;		// From 11-08-2014 to 03-12-2014
 	var daysFeatureComplete = 487;	// From 11-08-2014 to 10-12-2015
 	var daysAllTestsRun2 = 543;		// From 11-08-2014 to 04-02-2016
 	var daysZeroBugs = 620;			// From 11-08-2014 to 21-04-2016
 	var daysFinalRc = 711;			// From 11-08-2014 to 21-07-2016
 
+	$('.divider.jdk-modular').css({ left: (daysJdkModular * 100 / totalDaysProgress) + '%' });
 	$('.divider.feature-complete').css({ left: (daysFeatureComplete * 100 / totalDaysProgress) + '%' });
 	$('.divider.all-tests-run2').css({ left: (daysAllTestsRun2 * 100 / totalDaysProgress) + '%' });
 	$('.divider.zero-bug-bounce').css({ left: (daysZeroBugs * 100 / totalDaysProgress) + '%' });
 	$('.divider.final-rc').css({ left: (daysFinalRc * 100 / totalDaysProgress) + '%' });
 	
-		
-	$('a.twitter-share-button').attr('data-text', 'Only ' + days + ' days left until #Java9 is released!');
+	var tweetText = 'Only ' + days + ' days left until #Java9 is released!';
+	$('a.twitter-share-button').attr('data-text', tweetText);
 	$('.tweet-this').attr('href', 'https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flocalhost%3A8082%2F&ref_src=twsrc%5Etfw&text=Only%20' + days + '%20days%20left%20until%20%23Java9%20is%20released!&tw_p=tweetbutton&url=http%3A%2F%2Fjava9.xyz&via=takipid')
 	
 	
@@ -56,11 +58,24 @@ $(function() {
 		})
 	});
 	
+	/* Post container click handler */
+	$('.post-container').click(function(e) {
+		var link = $(this).find('a.read-more').attr('href');
+		window.open(link);
+	});
+	
 	setTimeout(function() {
-		$(".social").load('social.html');
-		setTimeout(function() {
-			$(".social").fadeTo(500, 1);
-		}, 2000)
+		var $social = $("<div>");
+		$social.load('social.html', function() {
+			$(this).find('a.twitter-share-button').attr('data-text', tweetText);
+			var $this = $(this);
+			setTimeout(function() {
+				$('#container .social').html($this.html());
+				$('#container .social').fadeTo(500, 1);
+			}, 2000);
+		});
+		
+		
 	}, 2000);
 	
 });
